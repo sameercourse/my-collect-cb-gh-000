69a6376a2253ee6d8881b1394773e967d331bf58
sameer shaik <sameercourse@gmail.com>
2018-11-20 14:36:32 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(arr)
+
+counter=0;
+collections=[];
+while counter<arr.length do
+  
+  collections<< yield(arr[counter])
+  counter+=1
+
+end 
+collections
+end 
 
 
+my_collect(["Tim Jones","Tom Smith","Jim Campagno"]) do |str|
+  
+  str.split(" ").first 
+  
+end
+ 
