9d425125a28dc710ab65110b37c219ad5893e957
sameer shaik <sameercourse@gmail.com>
2018-11-20 14:36:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,25 @@
+def my_collect(arr)
+
+counter=0;
+while counter<arr.length do
+  
+  yield(arr[counter])
 
 
+
+end 
+
+
+ 
+
+
+
+end 
+
+
+my_collect(["Tim Jones","Tom Smith","Jim Campagno"]) do |str|
+  
+  str.split(" ").first 
+  
+end
+ 
